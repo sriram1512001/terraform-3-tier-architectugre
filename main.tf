39dfe51552fe2bb1e12e4545055d10f665adb732
sriram1512001 <srichoubey555@gmail.com>
2025-12-06 03:30:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -277,30 +277,7 @@ resource "aws_security_group" "appserver-sg" {
 }
 
 # Create Database Security Group
-resource "aws_security_group" "database-sg" {
-  name        = "Database-SG"
-  description = "Allow inbound traffic from application layer"
-  vpc_id      = aws_vpc.my-vpc.id
 
-  ingress {
-    description     = "Allow traffic from application layer"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 32768
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Database-SG"
-  }
-}
 
 resource "aws_lb" "external-elb" {
   name               = "External-LB"
